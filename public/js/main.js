da033482aeb7a4421e50f75d9d5821943b1d7c69
Jonathan <jonathan.arlevy@gmail.com>
2019-01-06 15:44:54 -0500

    Form data: - Getting data & fields event

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
+let userInfosController = {
+  // Value of the user form
+  values: [],
+  // Setup the user infos
+  // and the behaviour of the form
+  init: () => {
+    userInfosController.updateValues();
+    $(".pace_vars").each((i, el) => {
+      let _el = $(el);
+
+      _el.on("change", () => userInfosController.updateValues());
+    });
+  },
+  // Updates all values from
+  // user form
+  updateValues: () => {
+    userInfosController.values = [];
+    $(".pace_vars").each((i, el) => {
+      let _el = $(el);
+
+      userInfosController.values.push(_el.val());
+    });
+    console.log(userInfosController.values);
+  }
+};
 $(function () {
-  console.log("Reveal says hello 👋");
+  userInfosController.init();
 });
